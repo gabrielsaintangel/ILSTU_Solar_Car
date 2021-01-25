936c97a9d8c94279966777f0ed0d571b26e2b1e6
gabe <gabestangel70@gmail.com>
2021-01-25 17:34:28 -0600

    work work

--- embedded software/telemetry/telemetry.ino
+++ embedded software/telemetry/telemetry.ino
@@ -5,13 +5,41 @@
 
 // variables
 unsigned char messageBuffer[8]; // for the CAN messages
+unsigned long lastMillis = 0;
+unsigned long lastMessageSentMillis = 0;
+
+// pins
+const int stopButtonIn = 4; // stop button in pin
+const int canIn = A0; // CAN in pin
+
+    // extra pins, not used currently
+const int outputPin1 = 5;
+const int outputPin1 = 6;
+const int outputPin1 = 7;
+const int outputPin1 = 8;
+const int outputPin1 = 9;
+
+
+// constants
+const int loopDelay = 100; // main loop delay in ms
+const int checkWait = 60000; // wait 60 sec before connection with car is deemed unreachable
+const int lightFlashPeriod = 500; // lights flash at this rate in ms, per solar car regulations
+
 
 void stopButtonPressed()
 {
 
 }
 
+// flashes the lights in a recognizable pattern in case of an error
+void flashErrorLights(){
+  
+}
+
 void manageLights(unsigned char messageBuffer)
+{
+  
+}
 
 // this method reads incoming can messages from the car
 void readCan()
